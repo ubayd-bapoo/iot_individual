e38a4ce4f3a2d3f7659da55b3b3fb1e0585f9203
Ubayd Bapoo <ubayd.bapoo@gmail.com>
2022-04-18 15:45:09 +0200

    Checking change

--- mqtt_client.py
+++ mqtt_client.py
@@ -6,7 +6,7 @@ broker = '10.0.2.2'
 client = mqtt.Client('test1')
 print('Connecting to broker:', broker)
 client.connect(broker)
-client.disconnect()
+
 
 sense = SenseHat()
 stored_temperature = round(sense.get_temperature(), 0)
@@ -14,11 +14,12 @@ stored_temperature = round(sense.get_temperature(), 0)
 while True:
     temperature = round(sense.get_temperature(), 0)
     if temperature != stored_temperature:
-        print(temperature, stored_temperature)
         stored_temperature = temperature
-    #     if temperature > 34:
-    #         print('hot')
-    #     elif 34 > temperature > 24:
-    #         print('medium')
-    #     else:
-    #         print('cold')
+        if temperature > 34:
+            print('hot')
+        elif 34 > temperature > 24:
+            print('medium')
+        else:
+            print('cold')
+
+client.disconnect()

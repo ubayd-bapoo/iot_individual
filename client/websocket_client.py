8a6f2274ec345d8616485686c8d67920dfa9bfb6
Ubayd Bapoo <ubayd.bapoo@gmail.com>
2022-04-29 23:06:46 +0200

    running client in loop

--- client/websocket_client.py
+++ client/websocket_client.py
@@ -9,34 +9,30 @@ from sense_emu import SenseHat
 HOST = '10.0.2.2'
 PORT = 8000
 
-async def produce(message: str, host:str, port: int) -> None:
-    async with websockets.connect(f"ws://{host}:{port}/ws/sensehat/") as ws:
-        await ws.send(message)
-        await ws.recv()
-        await ws.disconnect()
 
-def start_sensehat():
+async def start_sensehat(host: str, port: int) -> None:
     sense = SenseHat()
     stored_temperature = round(sense.get_temperature(), 0)
 
-    while True:
-        temperature = round(sense.get_temperature(), 0)
-        if temperature != stored_temperature:
-            stored_temperature = temperature
-            if temperature > 34:
-                message = {'temperature': temperature, 'type': 'hot'}
-                print(f'Writing Message {message}')
-                asyncio.get_event_loop().run_until_complete(produce(json.dumps(message), HOST, PORT))
-            elif 34 > temperature > 24:
-                message = {'temperature': temperature, 'type': 'medium'}
-                print(f'Writing Message {message}')
-                asyncio.get_event_loop().run_until_complete(produce(json.dumps(message), HOST, PORT))
-            else:
-                message = {'temperature': temperature, 'type': 'cold'}
-                print(f'Writing Message {message}')
-                asyncio.get_event_loop().run_until_complete(produce(json.dumps(message), HOST, PORT))
-            time.sleep(3)
+    async with websockets.connect(f"ws://{host}:{port}/ws/sensehat/") as ws:
+        while True:
+            temperature = round(sense.get_temperature(), 0)
+            if temperature != stored_temperature:
+                stored_temperature = temperature
+                if temperature > 34:
+                    message = {'temperature': temperature, 'type': 'hot'}
+                    print(f'Writing Message {message}')
+                    ws.send(json.dumps(message))
+                elif 34 > temperature > 24:
+                    message = {'temperature': temperature, 'type': 'medium'}
+                    print(f'Writing Message {message}')
+                    ws.send(json.dumps(message))
+                else:
+                    message = {'temperature': temperature, 'type': 'cold'}
+                    print(f'Writing Message {message}')
+                    ws.send(json.dumps(message))
+                time.sleep(3)
 
 
 if __name__ == "__main__":
-    start_sensehat()
+    asyncio.get_event_loop().run_until_complete(start_sensehat(HOST, PORT))

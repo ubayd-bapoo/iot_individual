71ac38e41b4331e1c2bd5a04085ac9db4d29804c
Ubayd Bapoo <ubayd.bapoo@gmail.com>
2022-04-29 22:27:02 +0200

    Adding websocket client

---
+++ client/websocket_client.py
@@ -0,0 +1,41 @@
+import asyncio
+import json
+import time
+import websockets
+
+from sense_emu import SenseHat
+
+
+HOST = '10.0.2.2'
+PORT = 8000
+
+async def produce(message: str, host:str, port: int) -> None:
+    async with websockets.connect(f"ws://{host}:{port}") as ws:
+        await ws.send(message)
+        await ws.recv()
+
+def start_sensehat():
+    sense = SenseHat()
+    stored_temperature = round(sense.get_temperature(), 0)
+
+    while True:
+        temperature = round(sense.get_temperature(), 0)
+        if temperature != stored_temperature:
+            stored_temperature = temperature
+            if temperature > 34:
+                message = {'temp': temperature, 'type': 'hot'}
+                print(f'Writing Message {message}')
+                produce(json.dumps(message), HOST, PORT)
+            elif 34 > temperature > 24:
+                message = {'temp': temperature, 'type': 'medium'}
+                print(f'Writing Message {message}')
+                produce(json.dumps(message), HOST, PORT)
+            else:
+                message = {'temp': temperature, 'type': 'cold'}
+                print(f'Writing Message {message}')
+                produce(json.dumps(message), HOST, PORT)
+            time.sleep(3)
+
+
+if __name__ == "__main__":
+    start_sensehat()
